96ee76ac26f085fc5a233a2b6c22f2e4e9efe33f
Doreen Hakimi <doreen.hakimi@gmail.com>
2016-05-30 02:26:34 -0700

    oops

--- scripts/clear-preview.sh
+++ scripts/clear-preview.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-grep -rl 'a(href="/")' src/ | xargs sed -i '' 's/a(href="\/")/a(href="\/early-career-toolkit\/").md/g'
+grep -rl 'a(href="/")' src/ | xargs sed -i '' 's/a(href="\/")/a(href="\/early-career-toolkit\/")/g'
 rm -rf src/content/
 rm src/index.md
