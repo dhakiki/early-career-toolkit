3c1ca1ca57ed84cbccd1245082ea395fd2d61076
Doreen Hakimi <doreen.hakimi@gmail.com>
2016-05-30 02:17:10 -0700

    contributing should still have .md but didnt work locally

--- scripts/preview.sh
+++ scripts/preview.sh
@@ -5,4 +5,5 @@ cp -f README.md src/index.md
 cp -Rf content/ src/content/
 grep -rl '.md' src/ | xargs sed -i '' 's/.md//g'
 grep -rl '/early-career-toolkit/' src/ | xargs sed -i '' 's/early-career-toolkit\///g'
+grep -rl 'CONTRIBUTING' src/ | xargs sed -i '' 's/CONTRIBUTING/CONTRIBUTING.md/g'
 $(npm bin)/harp server src
